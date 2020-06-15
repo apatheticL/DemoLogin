64127a7b120e49e855ea1974f70241d6e950c313
le.le <le.le@sea-solutions.com>
2020-06-15 17:43:20 +0700

    first commit hihi

---
+++ app/src/main/java/com/example/loginbyfacebookongoogle/LoginDifferent.java
@@ -0,0 +1,87 @@
+package com.example.loginbyfacebookongoogle;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.zxing.common.StringUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+public class LoginDifferent {
+    public static final int RC_SIGN_IN = 123;
+    private Context context;
+    private Activity activity;
+    private CallbackManager callbackManager;
+    private GoogleSignInClient googleSignInClient;
+    public LoginDifferent(Context context,
+                          Activity activity, CallbackManager callbackManager,
+                          GoogleSignInClient googleSignInClient) {
+        this.context = context;
+        this.activity = activity;
+        this.callbackManager = callbackManager;
+        this.googleSignInClient = googleSignInClient;
+    }
+
+
+    public void faceBookLogin() {
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().logInWithReadPermissions(activity, Arrays.asList("public_profile", "email", "user_birthday"));
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    String name = object.getString("name");
+                                    String email = object.getString("email");
+                                    if (email.equals("")) {
+                                        Toast.makeText(context, "Facebook ko co email", Toast.LENGTH_LONG).show();
+                                    }
+                                    Toast.makeText(context, name, Toast.LENGTH_LONG).show();
+                                    Log.d("facebook", name);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("MainActivity", "@@@onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.i("MainActivity", "@@@onError: " + error.getMessage());
+            }
+        });
+    }
+
+    public void googleLogin() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        activity.startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+}
---
+++ app/src/main/java/com/example/loginbyfacebookongoogle/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.loginbyfacebookongoogle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.buttonLoginFace)
+    Button buttonLoginWithFace;
+    @BindView(R.id.buttonLoginGoogle)
+    Button buttonLoginWithGoogle;
+    private LoginDifferent loginDifferent;
+    private CallbackManager callbackManager;
+    private GoogleSignInClient googleSignInClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_fragment);
+        ButterKnife.bind(this);
+        createRepuest();
+        callbackManager =  CallbackManager.Factory.create();
+        loginDifferent = new LoginDifferent(this, this, callbackManager,googleSignInClient);
+        buttonLoginWithFace.setOnClickListener(this);
+        buttonLoginWithGoogle.setOnClickListener(this);
+    }
+    private void createRepuest() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonLoginFace: {
+                loginWithFacebook();
+                break;
+            }
+            case R.id.buttonLoginGoogle:{
+                loginWithGoogle();
+                break;
+            }}
+
+    }
+
+    private void loginWithGoogle() {
+        loginDifferent.googleLogin();
+    }
+
+    private void loginWithFacebook() {
+        loginDifferent.faceBookLogin();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode== loginDifferent.RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                String accountData = account.getDisplayName()+ " " +account.getEmail() + account.getIdToken();
+                Toast.makeText(this,accountData,Toast.LENGTH_SHORT).show();
+            }catch (ApiException e){
+                Toast.makeText(this,e.getMessage().toString(),Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        else {
+            callbackManager.onActivityResult(requestCode,resultCode,data);
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
